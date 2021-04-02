bcfcfc1746a35efc97e8fcd247086017ee932584
Aleksa Helerr <aleksaheler@gmail.com>
2021-04-02 09:29:03 +0200

    Draw rainbow and display running time

--- src/main.cpp
+++ src/main.cpp
@@ -25,28 +25,37 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  /* DRAW A RAINBOW AND SOME TEXT */
+  /* Make the window */
   screen(WIDTH, HEIGHT, FULLSCREEN, TITLE);
 
-  for(int x = 0; x < w; x++)
-  for(int y = 0; y < h; y++)
+  /* While user doesnt exit program (esc or close button)*/
+  while(!done())
   {
-    //pset(x, y, ColorRGB(0, 128 + 128 * sin(x / 8.0), 0));
-    pset(x, y, ColorRGB(256*x / w, 256*y / h, 128));
-  }
+    /* Draw part of the color palette*/
+    for(int x = 0; x < w; x++)
+      for(int y = 0; y < h; y++)
+      {
+        pset(x, y, ColorRGB(256*x / w, 256*y / h, 128));
+      }
+    
+    /* Put some textx on the screen */
+    //print("Hello, world!", 16, 16);
+    print(getTime(), 16, 16);
+
+    std::string test;
+    test.resize(20);
+
+    /* Draw a face */
+    /*screen(256,256, 0, "A Face!"); 
+    drawDisk(128, 128, 100, ColorRGB(255, 128, 200)); 
+    drawDisk(88, 100, 10, ColorRGB(0, 0, 255)); 
+    drawDisk(168, 100, 10, ColorRGB(0, 0, 255)); 
+    drawLine(88, 150, 168, 150, ColorRGB(255, 0, 0));*/
+
+    /* Send frame to GPU to be displayed */
+    redraw();
+  } /* End of main loop */
   
-  print("Hello, world!", 16, 16);
-
-  std::string test;
-  test.resize(20);
-
-  /* DRAW A FACE */
-  /*screen(256,256, 0, "A Face!"); 
-  drawDisk(128, 128, 100, ColorRGB(255, 128, 200)); 
-  drawDisk(88, 100, 10, ColorRGB(0, 0, 255)); 
-  drawDisk(168, 100, 10, ColorRGB(0, 0, 255)); 
-  drawLine(88, 150, 168, 150, ColorRGB(255, 0, 0));*/
-
-  redraw();
-  sleep();
+  /* Wait for user input */
+  //sleep();
 }
