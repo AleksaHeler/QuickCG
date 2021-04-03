401e8f1dcb2cce1dee24d0c0f3c816c6c46b8b9f
aleksaheler <aleksaheler@gmail.com>
2021-04-03 18:22:56 +0200

    Add raycaster examples with floor and with sprites

--- src/raycaster_floor.cpp
+++ src/raycaster_floor.cpp
@@ -26,7 +26,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <vector>
 #include <iostream>
 
-#include "quickcg/quickcg.h"
+#include "quickcg.h"
 using namespace QuickCG;
 
 /*
--- src/raycaster_sprite.cpp
+++ src/raycaster_sprite.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2004-2019, Lode Vandevenne
+Copyright (c) 2004-2020, Lode Vandevenne
 
 All rights reserved.
 
@@ -34,9 +34,6 @@ g++ *.cpp -lSDL -O3 -W -Wall -ansi -pedantic
 g++ *.cpp -lSDL
 */
 
-// set to 1 to use the horizontal floor algorithm (contributed by Ádám Tóth in 2019),
-// or to 0 to use the slower vertical floor algorithm.
-#define FLOOR_HORIZONTAL 1
 
 #define screenWidth 640
 #define screenHeight 480
@@ -45,7 +42,7 @@ g++ *.cpp -lSDL
 #define mapWidth 24
 #define mapHeight 24
 
-int worldMap[mapWidth][mapHeight]=
+int worldMap[mapWidth][mapHeight] =
 {
   {8,8,8,8,8,8,8,8,8,8,8,4,4,6,4,4,6,4,6,4,4,4,6,4},
   {8,0,0,0,0,0,0,0,0,0,8,4,0,0,0,0,0,0,0,0,0,0,0,4},
@@ -73,7 +70,54 @@ int worldMap[mapWidth][mapHeight]=
   {2,2,2,2,1,2,2,2,2,2,2,1,2,2,2,5,5,5,5,5,5,5,5,5}
 };
 
+struct Sprite
+{
+  double x;
+  double y;
+  int texture;
+};
+
+#define numSprites 19
+
+Sprite sprite[numSprites] =
+{
+  {20.5, 11.5, 10}, //green light in front of playerstart
+  //green lights in every room
+  {18.5,4.5, 10},
+  {10.0,4.5, 10},
+  {10.0,12.5,10},
+  {3.5, 6.5, 10},
+  {3.5, 20.5,10},
+  {3.5, 14.5,10},
+  {14.5,20.5,10},
+
+  //row of pillars in front of wall: fisheye test
+  {18.5, 10.5, 9},
+  {18.5, 11.5, 9},
+  {18.5, 12.5, 9},
+
+  //some barrels around the map
+  {21.5, 1.5, 8},
+  {15.5, 1.5, 8},
+  {16.0, 1.8, 8},
+  {16.2, 1.2, 8},
+  {3.5,  2.5, 8},
+  {9.5, 15.5, 8},
+  {10.0, 15.1,8},
+  {10.5, 15.8,8},
+};
+
 Uint32 buffer[screenHeight][screenWidth]; // y-coordinate first because it works per scanline
+
+//1D Zbuffer
+double ZBuffer[screenWidth];
+
+//arrays used to sort the sprites
+int spriteOrder[numSprites];
+double spriteDistance[numSprites];
+
+//function used to sort the sprites
+void sortSprites(int* order, double* dist, int amount);
 
 int main(int /*argc*/, char */*argv*/[])
 {
@@ -84,8 +128,8 @@ int main(int /*argc*/, char */*argv*/[])
   double time = 0; //time of current frame
   double oldTime = 0; //time of previous frame
 
-  std::vector<Uint32> texture[8];
-  for(int i = 0; i < 8; i++) texture[i].resize(texWidth * texHeight);
+  std::vector<Uint32> texture[11];
+  for(int i = 0; i < 11; i++) texture[i].resize(texWidth * texHeight);
 
   screen(screenWidth,screenHeight, 0, "Raycaster");
 
@@ -101,10 +145,15 @@ int main(int /*argc*/, char */*argv*/[])
   error |= loadImage(texture[7], tw, th, "pics/colorstone.png");
   if(error) { std::cout << "error loading images" << std::endl; return 1; }
 
+  //load some sprite textures
+  error |= loadImage(texture[8], tw, th, "pics/barrel.png");
+  error |= loadImage(texture[9], tw, th, "pics/pillar.png");
+  error |= loadImage(texture[10], tw, th, "pics/greenlight.png");
+  if(error) { std::cout << "error loading images" << std::endl; return 1; }
+
   //start the main loop
   while(!done())
   {
-#if FLOOR_HORIZONTAL
     //FLOOR CASTING
     for(int y = screenHeight / 2 + 1; y < screenHeight; ++y)
     {
@@ -118,26 +167,10 @@ int main(int /*argc*/, char */*argv*/[])
       int p = y - screenHeight / 2;
 
       // Vertical position of the camera.
-      // NOTE: with 0.5, it's exactly in the center between floor and ceiling,
-      // matching also how the walls are being raycasted. For different values
-      // than 0.5, a separate loop must be done for ceiling and floor since
-      // they're no longer symmetrical.
       float posZ = 0.5 * screenHeight;
 
       // Horizontal distance from the camera to the floor for the current row.
       // 0.5 is the z position exactly in the middle between floor and ceiling.
-      // NOTE: this is affine texture mapping, which is not perspective correct
-      // except for perfectly horizontal and vertical surfaces like the floor.
-      // NOTE: this formula is explained as follows: The camera ray goes through
-      // the following two points: the camera itself, which is at a certain
-      // height (posZ), and a point in front of the camera (through an imagined
-      // vertical plane containing the screen pixels) with horizontal distance
-      // 1 from the camera, and vertical position p lower than posZ (posZ - p). When going
-      // through that point, the line has vertically traveled by p units and
-      // horizontally by 1 unit. To hit the floor, it instead needs to travel by
-      // posZ units. It will travel the same ratio horizontally. The ratio was
-      // 1 / p for going through the camera plane, so to go posZ times farther
-      // to reach the floor, we get that the total horizontal distance is posZ / p.
       float rowDistance = posZ / p;
 
       // calculate the real world step vector we have to add for each x (parallel to camera plane)
@@ -181,7 +214,6 @@ int main(int /*argc*/, char */*argv*/[])
         buffer[screenHeight - y - 1][x] = color;
       }
     }
-#endif // FLOOR_HORIZONTAL
 
     // WALL CASTING
     for(int x = 0; x < w; x++)
@@ -269,8 +301,8 @@ int main(int /*argc*/, char */*argv*/[])
 
       //calculate value of wallX
       double wallX; //where exactly the wall was hit
-      if(side == 0) wallX = posY + perpWallDist * rayDirY;
-      else          wallX = posX + perpWallDist * rayDirX;
+      if (side == 0) wallX = posY + perpWallDist * rayDirY;
+      else           wallX = posX + perpWallDist * rayDirX;
       wallX -= floor((wallX));
 
       //x coordinate on the texture
@@ -294,64 +326,77 @@ int main(int /*argc*/, char */*argv*/[])
         buffer[y][x] = color;
       }
 
-#if !FLOOR_HORIZONTAL
-      //FLOOR CASTING
-      double floorXWall, floorYWall; //x, y position of the floor texel at the bottom of the wall
+      //SET THE ZBUFFER FOR THE SPRITE CASTING
+      ZBuffer[x] = perpWallDist; //perpendicular distance is used
+    }
 
-      //4 different wall directions possible
-      if(side == 0 && rayDirX > 0)
+    //SPRITE CASTING
+    //sort sprites from far to close
+    for(int i = 0; i < numSprites; i++)
+    {
+      spriteOrder[i] = i;
+      spriteDistance[i] = ((posX - sprite[i].x) * (posX - sprite[i].x) + (posY - sprite[i].y) * (posY - sprite[i].y)); //sqrt not taken, unneeded
+    }
+    sortSprites(spriteOrder, spriteDistance, numSprites);
+
+    //after sorting the sprites, do the projection and draw them
+    for(int i = 0; i < numSprites; i++)
+    {
+      //translate sprite position to relative to camera
+      double spriteX = sprite[spriteOrder[i]].x - posX;
+      double spriteY = sprite[spriteOrder[i]].y - posY;
+
+      //transform sprite with the inverse camera matrix
+      // [ planeX   dirX ] -1                                       [ dirY      -dirX ]
+      // [               ]       =  1/(planeX*dirY-dirX*planeY) *   [                 ]
+      // [ planeY   dirY ]                                          [ -planeY  planeX ]
+
+      double invDet = 1.0 / (planeX * dirY - dirX * planeY); //required for correct matrix multiplication
+
+      double transformX = invDet * (dirY * spriteX - dirX * spriteY);
+      double transformY = invDet * (-planeY * spriteX + planeX * spriteY); //this is actually the depth inside the screen, that what Z is in 3D, the distance of sprite to player, matching sqrt(spriteDistance[i])
+
+      int spriteScreenX = int((w / 2) * (1 + transformX / transformY));
+
+      //parameters for scaling and moving the sprites
+      #define uDiv 1
+      #define vDiv 1
+      #define vMove 0.0
+      int vMoveScreen = int(vMove / transformY);
+
+      //calculate height of the sprite on screen
+      int spriteHeight = abs(int(h / (transformY))) / vDiv; //using "transformY" instead of the real distance prevents fisheye
+      //calculate lowest and highest pixel to fill in current stripe
+      int drawStartY = -spriteHeight / 2 + h / 2 + vMoveScreen;
+      if(drawStartY < 0) drawStartY = 0;
+      int drawEndY = spriteHeight / 2 + h / 2 + vMoveScreen;
+      if(drawEndY >= h) drawEndY = h - 1;
+
+      //calculate width of the sprite
+      int spriteWidth = abs( int (h / (transformY))) / uDiv;
+      int drawStartX = -spriteWidth / 2 + spriteScreenX;
+      if(drawStartX < 0) drawStartX = 0;
+      int drawEndX = spriteWidth / 2 + spriteScreenX;
+      if(drawEndX >= w) drawEndX = w - 1;
+
+      //loop through every vertical stripe of the sprite on screen
+      for(int stripe = drawStartX; stripe < drawEndX; stripe++)
       {
-        floorXWall = mapX;
-        floorYWall = mapY + wallX;
+        int texX = int(256 * (stripe - (-spriteWidth / 2 + spriteScreenX)) * texWidth / spriteWidth) / 256;
+        //the conditions in the if are:
+        //1) it's in front of camera plane so you don't see things behind you
+        //2) it's on the screen (left)
+        //3) it's on the screen (right)
+        //4) ZBuffer, with perpendicular distance
+        if(transformY > 0 && stripe > 0 && stripe < w && transformY < ZBuffer[stripe])
+        for(int y = drawStartY; y < drawEndY; y++) //for every pixel of the current stripe
+        {
+          int d = (y-vMoveScreen) * 256 - h * 128 + spriteHeight * 128; //256 and 128 factors to avoid floats
+          int texY = ((d * texHeight) / spriteHeight) / 256;
+          Uint32 color = texture[sprite[spriteOrder[i]].texture][texWidth * texY + texX]; //get current color from the texture
+          if((color & 0x00FFFFFF) != 0) buffer[y][stripe] = color; //paint pixel if it isn't black, black is the invisible color
+        }
       }
-      else if(side == 0 && rayDirX < 0)
-      {
-        floorXWall = mapX + 1.0;
-        floorYWall = mapY + wallX;
-      }
-      else if(side == 1 && rayDirY > 0)
-      {
-        floorXWall = mapX + wallX;
-        floorYWall = mapY;
-      }
-      else
-      {
-        floorXWall = mapX + wallX;
-        floorYWall = mapY + 1.0;
-      }
-
-      double distWall, distPlayer, currentDist;
-
-      distWall = perpWallDist;
-      distPlayer = 0.0;
-
-      if(drawEnd < 0) drawEnd = h; //becomes < 0 when the integer overflows
-
-      //draw the floor from drawEnd to the bottom of the screen
-      for(int y = drawEnd + 1; y < h; y++)
-      {
-        currentDist = h / (2.0 * y - h); //you could make a small lookup table for this instead
-
-        double weight = (currentDist - distPlayer) / (distWall - distPlayer);
-
-        double currentFloorX = weight * floorXWall + (1.0 - weight) * posX;
-        double currentFloorY = weight * floorYWall + (1.0 - weight) * posY;
-
-        int floorTexX, floorTexY;
-        floorTexX = int(currentFloorX * texWidth) & (texWidth - 1);
-        floorTexY = int(currentFloorY * texHeight) & (texHeight - 1);
-
-        int checkerBoardPattern = ((int)currentFloorX + (int)currentFloorY) & 1;
-        int floorTexture;
-        if(checkerBoardPattern == 0) floorTexture = 3;
-        else floorTexture = 4;
-
-        //floor
-        buffer[y][x] = (texture[floorTexture][texWidth * floorTexY + floorTexX] >> 1) & 8355711;
-        //ceiling (symmetrical)
-        buffer[h - y][x] = texture[6][texWidth * floorTexY + floorTexX];
-      }
-#endif // !FLOOR_HORIZONTAL
     }
 
     drawBuffer(buffer[0]);
@@ -402,5 +447,25 @@ int main(int /*argc*/, char */*argv*/[])
       planeX = planeX * cos(rotSpeed) - planeY * sin(rotSpeed);
       planeY = oldPlaneX * sin(rotSpeed) + planeY * cos(rotSpeed);
     }
+    if(keyDown(SDLK_ESCAPE))
+    {
+      break;
+    }
+  }
+}
+
+//sort the sprites based on distance
+void sortSprites(int* order, double* dist, int amount)
+{
+  std::vector<std::pair<double, int>> sprites(amount);
+  for(int i = 0; i < amount; i++) {
+    sprites[i].first = dist[i];
+    sprites[i].second = order[i];
+  }
+  std::sort(sprites.begin(), sprites.end());
+  // restore in reverse order to go from farthest to nearest
+  for(int i = 0; i < amount; i++) {
+    dist[i] = sprites[amount - i - 1].first;
+    order[i] = sprites[amount - i - 1].second;
   }
 }
